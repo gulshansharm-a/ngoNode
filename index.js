b3b407d2255b08a1f85c6c7eaf923f6bb729fb44
Gulshan <arun123gulshan@gmail.com>
2023-07-16 13:30:19 +0530

    ibsert from data base

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const http = require('http');
 const url = require('url');
+const mysql = require('mysql2');
+const http = require('http');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -8,10 +9,21 @@ const connection = mysql.createConnection({
   database: 'test'
 });
 
-
 const server = http.createServer((req, res) => {
   // Set the response content type to JSON
   res.setHeader('Content-Type', 'application/json');
+
+  // Set CORS headers to allow requests from any origin
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    // Preflight request, respond with 200 OK
+    res.statusCode = 200;
+    res.end();
+    return;
+  }
 
   const parsedUrl = url.parse(req.url, true);
   const queryParameters = parsedUrl.query;
@@ -41,44 +53,50 @@ const server = http.createServer((req, res) => {
       agentDetail : "hs ls kjsh lamaj jak ua j akj fjf tesdrtfij  aksjdhf jdhf uasj"
     };
     res.end(JSON.stringify(responseData));
-  } else 
-  if (parsedUrl.pathname === '/student-profile/add') {
-    const createTableSQL = `
-      CREATE TABLE IF NOT EXISTS Student (
-        ID int NOT NULL PRIMARY KEY,
-        aadharNumber VARCHAR(255),
-        studentName VARCHAR(255),
-        fatherName VARCHAR(255),
-        motherName VARCHAR(255),
-        gender VARCHAR(255),
-        DOB VARCHAR(255),
-        schoolName VARCHAR(255),
-        areaType VARCHAR(255),
-        mobile VARCHAR(255),
-        email VARCHAR(255),
-        villageOrTown VARCHAR(255),
-        pinCode VARCHAR(255),
-        district VARCHAR(255),
-        state VARCHAR(255),
-        fatherAadharNo VARCHAR(255),
-        motherAadharNo VARCHAR(255),
-        fatherPanNo VARCHAR(255),
-        enrollDate VARCHAR(255),
-        agentDetail VARCHAR(1055)
-      )
-    `;
+  } else if (parsedUrl.pathname === '/student-profile/add') {
+    const ID = queryParameters.studentID;
+    const aadharNumber = queryParameters.aadharNumber;
+    const studentName = queryParameters.studentName;
+    const fatherName = queryParameters.fatherName;
+    const motherName = queryParameters.motherName;
+    const gender = queryParameters.gender;
+    const DOB = queryParameters.DOB;
+    const schoolName = queryParameters.schoolName;
+    const areaType = queryParameters.areaType;
+    const mobile = queryParameters.mobile;
+    const email = queryParameters.email;
+    const villageOrTown = queryParameters.villageOrTown;
+    const pinCode = queryParameters.pinCode;
+    const district = queryParameters.district;
+    const state = queryParameters.state;
+    const fatherAadharNo = queryParameters.fatherAadharNo;
+    const motherAadharNo = queryParameters.motherAadharNo;
+    const fatherPanNo = queryParameters.fatherPanNo;
+    const enrollDate = queryParameters.enrollDate;
+    const agentDetail = queryParameters.agentDetail;
 
-    connection.query(createTableSQL, (err, results) => {
-      if (err) {
-        console.error('Error creating table:', err);
-        res.statusCode = 500;
-        res.end(JSON.stringify({ error: 'Error creating table' }));
-        return;
-      }
-      console.log('Table created successfully');
+    console.log('Inserting data:', {
+      ID,
+      aadharNumber,
+      studentName,
+      fatherName,
+      motherName,
+      gender,
+      DOB,
+      schoolName,
+      areaType,
+      mobile,
+      email,
+      villageOrTown,
+      pinCode,
+      district,
+      state,
+      fatherAadharNo,
+      motherAadharNo,
+      fatherPanNo,
+      enrollDate,
+      agentDetail
     });
-
-    console.log('Inserting data:', { ID, aadharNumber, studentName, /* ... */ });
 
     const insertDataSQL = `
       INSERT INTO Student 
@@ -115,10 +133,9 @@ const server = http.createServer((req, res) => {
         return;
       }
       console.log('Data inserted successfully');
+      res.statusCode = 200;
+      res.end(JSON.stringify({ message: 'Data inserted successfully' }));
     });
-
-    res.statusCode = 200;
-    res.end(JSON.stringify({ message: 'Data inserted successfully' }));
   } else {
     // Handle other requests
     res.statusCode = 404; // Not Found
